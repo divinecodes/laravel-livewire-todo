7cd7ce00ffd195e95560288663afae440b47d648
Divine Tettey <tetteydivine0@gmail.com>
2020-10-26 16:01:34 +0000

    initializing project with laravel livewire

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Livewire Test</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        @livewireStyles
+    </head>
+    <body>
+        @yield('content')
+
+        @livewireScripts
+    </body>
+</html>
